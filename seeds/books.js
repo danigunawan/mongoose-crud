668587a0392b06a15035e86c882294c333d75976
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-06 16:59:56 +0700

    Books Seeder

--- seeds/books.js
+++ seeds/books.js
@@ -1,46 +1,48 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
-const dbName = 'library';
-let client;
+const mongoose = require('mongoose');
+const url = "mongodb://localhost/library_mongoose";
+mongoose.connect(url,(err) => {
+  if (!err) {
+    console.log('Success Connect To Database!');
+  }
+});
+const Book = require('../models/books');
 
-const runSeeder =  () => {
-  (async function() {
-    try {
-      // Use connect method to connect to the Server
-      client = await MongoClient.connect(url);
-      const db = client.db(dbName);
+Book.remove({},(err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Remove All Books Collection');
+  }
+})
 
-      //remove all data
-      await db.collection('books').remove({},(err,removed) => {
-        console.log(`${removed} Books Data Removed`);
-      });
+let book = new Book({
+  isbn: '978-1-60309-057-5',
+  title: 'Dragon Puncher',
+  author: 'James Kochalka',
+  category: 'All Ages',
+  stock: 3
+});
 
-      // Insert two book
-      let r = await db.collection('books').insertMany([{
-        isbn: '978-1-60309-057-5',
-        title: 'Dragon Puncher',
-        author: 'James Kochalka',
-        category: 'All Ages',
-        stock: 3
-      },
-      {
-        isbn: '978-1-891830-77-8',
-        title: 'Every Girl is the end of the world for me',
-        author: 'Jeffrey Brown',
-        category: 'Mature (16+)',
-        stock: 5
-      }]);
-      const docs = await db.collection('books').find({}).toArray();
-      console.log(docs);
+book.save((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Success Add New Book');
+  }
+})
 
-    } catch (err) {
-      console.log(err.stack);
-    }
+book = new Book({
+  isbn: '978-1-891830-77-8',
+  title: 'Every Girl is the end of the world for me',
+  author: 'Jeffrey Brown',
+  category: 'Mature (16+)',
+  stock: 5
+});
 
-    if (client) {
-      client.close();
-    }
-  })();
-}
-
-runSeeder();
+book.save((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Success Add New Book');
+  }
+})
