dacc36dc5f11fc2d238361510d19bee6d656d90d
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-07 13:35:05 +0700

    Calculate due date and fine with middleware mongoose

--- models/transactions.js
+++ models/transactions.js
@@ -20,4 +20,34 @@ const transactionSchema = new Schema({
   }],
 });
 
+transactionSchema.pre('save', function(next) {
+  let due_date = new Date(this.out_date);
+  due_date.setDate(due_date.getDate() + this.days);
+  this.due_date = due_date;
+  this.fine = 0;
+  next();
+});
+
+transactionSchema.pre('findOneAndUpdate', function(next) {
+  let id = this._conditions._id;
+  let Transaction = this;
+  this.findOne({_id:id },(err,data) => {
+    let date1 = new Date(data.due_date);
+    let date2 = new Date(data.in_date);
+    let timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    let fine;
+    if (diffDays > 0) {
+      fine = diffDays * 1000;
+    }
+    Transaction.update({_id:id},{ $set: { fine: fine } },(err,data) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(data);
+    });
+  })
+  next();
+});
+
 module.exports = mongoose.model('Transaction',transactionSchema);
